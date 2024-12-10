5cd52d704eccadca49c1f6ee52430033b97def70
Brycycle <44349959+Bryce-Davidson@users.noreply.github.com>
2024-12-10 11:30:03 -0800

    chore: type conversion

--- tests/validate.R
+++ tests/validate.R
@@ -8,13 +8,15 @@ set.seed(11723)
 data_path <- "/Users/brycedavidson/Repos/iimi/data/model_data.rda"
 df <- get(load(data_path))
 label <- "detect_result"
+df$detect_result <- as.logical(df$detect_result) # Convert detect_result to logical
+
+# ----------------------------------------------------------------
+
+print(paste("Unique virus count:", length(unique(df$virus_name))))
 
 # ----------------------------------------------------------------
 
 F1_score <- function(y_true, y_pred) {
-    true <- as.logical(y_true)
-    pred <- as.logical(y_pred)
-
     precision <- sum(pred & true) / sum(pred)
     recall <- sum(pred & true) / sum(true)
     f1 <- 2 * (precision * recall) / (precision + recall)
@@ -33,10 +35,10 @@ for (i in 1:5) {
     train_df <- df[train_indices, ]
     test_df <- df[-train_indices, ]
 
-    train_y <- train_df$detect_result
+    train_y <- train_df[, label]
     train_x <- train_df[, -which(names(train_df) == label)]
 
-    test_y <- test_df$detect_result
+    test_y <- test_df[, label]
     test_x <- test_df[, -which(names(train_df) == label)]
 
     # -------------------------------
@@ -45,13 +47,17 @@ for (i in 1:5) {
 
     # -------------------------------
 
-    prediction <- predict_iimi(newdata = test_x, trained_model = fit, report_virus_level = FALSE)
+    prediction <- predict_iimi(newdata = test_x, trained_model = fit, report_virus_level = TRUE)
 
     # -------------------------------
 
+    test_y_virus <- test_df %>%
+        group_by(virus_name) %>%
+        summarize(detect_result = any(detect_result)) %>%
+        select(detect_result)
 
-    pred <- prediction$Prediction
-    f1_scores[i] <- F1_score(test_y, pred)
+    pred <- as.logical(prediction$Prediction)
+    f1_scores[i] <- F1_score(test_y_virus, pred)
 
     predictions[[i]] <- prediction
 }
