9c7585e09b4482f66bacdc15ff178507bbd53bb0
Shelley Gao <syugao@gmail.com>
2025-07-28 15:29:27 -0700

    add waldo::compare() snapshot to unreliable_regions_has_effect()
    use waldo::compare() on paired

--- tests/testthat/test-convert_rle_to_df.R
+++ tests/testthat/test-convert_rle_to_df.R
@@ -27,15 +27,18 @@ test_that("run_with_unreliable_regions", {
 })
 
 test_that("unreliable_regions_has_effect", {
-  expect_failure(
-    expect_equal(
-      convert_rle_to_df(
-        rles()
-      ),
-      convert_rle_to_df(
-        rles(),
-        unreliable_regions = prepared_unreliable_regions(),
-      )
+  unmasked_rles <- convert_rle_to_df(rles())
+  
+  masked_rles <- convert_rle_to_df(
+    rles(),
+    unreliable_regions = prepared_unreliable_regions(),
+  )
+
+  expect_snapshot(
+    waldo::compare(
+      unmasked_rles, masked_rles, 
+      max_diffs = Inf,
+      x_arg = "disabled", y_arg = "enabled"
     )
   )
 })
@@ -52,12 +55,22 @@ test_that("run_single", {
 })
 
 test_that("run_paired", {
-  rles <- convert_bam_to_rle(
+  rles_single <- convert_bam_to_rle(
+    here::here("tests", "files", "paired_1.sorted.bam"), 
+    paired = FALSE
+  )
+  
+  rles_paired <- convert_bam_to_rle(
     here::here("tests", "files", "paired_1.sorted.bam"), 
     paired = TRUE
   )
 
   expect_snapshot(
-    convert_rle_to_df(rles, unreliable_regions = prepared_unreliable_regions())
+    waldo::compare(
+      convert_rle_to_df(rles_single, unreliable_regions = prepared_unreliable_regions()),
+      convert_rle_to_df(rles_paired, unreliable_regions = prepared_unreliable_regions()), 
+      max_diffs = Inf,
+      x_arg = "single", y_arg = "paired"
+    )
   )
 })
